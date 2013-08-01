5bf67e8fae10eadbca98a7c3df63987f53591060
Farid Ali <f.ali20@gmail.com>
2013-08-01 02:35:35 -0400

    Users can view slide images on status and publishes view.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,22 @@
 class PicturesController < ApplicationController
+before_filter :authenticate_user!, only: [:create, :destroy]
+
+   def index
+      @status = Status.find(params[:status_id])
+      @slide = Slide.find(params[:slide_id])
+      @picture = Picture.all
+   end
+
   def create
     @slide = Slide.find(params[:slide_id])
     @picture = @slide.pictures.create(params[:picture])
     redirect_to :back
+  end
+
+  def show
+  @status = Status.find(params[:status_id])
+  @slide = Slide.find(params[:status_id])
+  redirect_to status_path
   end
 
   def destroy
