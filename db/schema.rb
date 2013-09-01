a957ef82d3a2da0d4c3b0bd399f378b66428cd5c
Farid Ali <f.ali20@gmail.com>
2013-09-01 12:27:57 -0400

    Styling changes made

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130813003655) do
+ActiveRecord::Schema.define(:version => 20130726204023) do
 
   create_table "pictures", :force => true do |t|
     t.string   "image"
