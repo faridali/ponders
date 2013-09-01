e4e6333bfc9d326019c6be7c7cbddff6a557ed8e
Farid Ali <f.ali20@gmail.com>
2013-09-01 12:50:39 -0400

    Removed migrations

--- db/migrate/20130726202541_remove_image_from_slides.rb
+++
@@ -1,9 +0,0 @@
-class RemoveImageFromSlides < ActiveRecord::Migration
-  def up
-    remove_column :slides, :image
-  end
-
-  def down
-    add_column :slides, :image, :string
-  end
-end
--- db/migrate/20130813003655_add_image_to_slides.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToSlides < ActiveRecord::Migration
-  def change
-    add_column :slides, :image, :string
-  end
-end
